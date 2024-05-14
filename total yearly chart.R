916e175357a89241f5088b60a8695a227c396e51
Prio Aji Reynaldi <163991169+prioaji@users.noreply.github.com>
2024-05-14 08:15:14 +0700

    Delete total yearly chart.R

--- total yearly chart.R
+++
@@ -1,3 +0,0 @@
-cust_retention %>%
-  ggplot(cust_retention, mapping=aes(x=year,y=number_of_trans)) + geom_col(fill = "darkgreen")+ 
-  labs(x='Year',y='Yearly Transaction',title = 'Number of Transaction per Year')
