8cc39350a7012a1ec58a46be147a4c0bf0f5a8dc
Prio Aji Reynaldi <163991169+prioaji@users.noreply.github.com>
2024-05-14 08:18:08 +0700

    Delete chart_ratio_of_value.R

--- chart_ratio_of_value.R
+++
@@ -1,7 +0,0 @@
-transaction %>% 
-  mutate(year=year(Date)) %>% 
-  group_by(year) %>% 
-  summarise(trans_value=sum(TotalSales),discount_value=sum(Discount)) %>% 
-  mutate(ratio=discount_value/trans_value) -> ratio_ 
-ratio_%>% 
-  ggplot(transaction, mapping=aes(x=year,y=ratio)) + geom_line(color="purple") + geom_point(color="purple") + labs(x='Year',y='Ratio',title = 'Ratio : Discount vs Value')
