97e9a6f2a3a9b3365554d19f0889a74ca2bdae03
Prio Aji Reynaldi <163991169+prioaji@users.noreply.github.com>
2024-05-14 08:16:05 +0700

    Delete function_maketable.R

--- function_maketable.R
+++
@@ -1,9 +0,0 @@
-maketable <- function(year1 = 2019){
-transaction %>% 
-  filter(year(Date) == year1) %>% 
-  select(CustomerID) %>% 
-  distinct(CustomerID) %>% 
-  mutate(counts = 1)
-}
-
-maketable(2020)
