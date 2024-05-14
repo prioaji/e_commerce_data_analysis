ca2d53a851e024b484814418e0b90d9de92ff50e
Prio Aji Reynaldi <163991169+prioaji@users.noreply.github.com>
2024-05-14 08:17:17 +0700

    Delete chart_trend_of_trans_value.R

--- chart_trend_of_trans_value.R
+++
@@ -1,9 +0,0 @@
-transaction %>% 
-  group_by(Date) %>% 
-  summarise(trans_value=sum(TotalSales),discount_value=sum(Discount)) -> trans
-  pivot_longer(trans,trans_value:discount_value, names_to = "value_type",values_to = "value") -> transaction_3
-
-transaction_3 %>% 
-  ggplot(transaction_3, mapping=aes(x=Date,y=value,color = value_type)) + 
-  geom_smooth() + 
-  labs(x='Year',y='Daily Transaction Value',title = 'The Trends of Transaction Value')
