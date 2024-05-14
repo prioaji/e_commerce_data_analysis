b6bccbb5187b7e275a9aef3d52f9abe8a7ff0e78
Prio Aji Reynaldi <163991169+prioaji@users.noreply.github.com>
2024-05-14 08:18:20 +0700

    Delete chart_discount_value_trend.R

--- chart_discount_value_trend.R
+++
@@ -1,4 +0,0 @@
-transaction %>% 
-  group_by(Date) %>% 
-  summarise(daily_discount = sum(Discount)) %>% 
-  ggplot(transaction, mapping = aes(x=Date, y=daily_discount)) + geom_line(color="green") + geom_smooth(color="blue")
